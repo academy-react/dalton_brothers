149997b9b1ab2b63999b12766954d200432acd5d
amin <amminn.ghaderi@gmail.com>
2023-10-03 14:11:12 +0330

    Article

--- dalton_brothers/src/Components/Article & News/Article & News.jsx
+++ dalton_brothers/src/Components/Article & News/Article & News.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
+import { Course } from '../Course/CourseSections/Course'
+import { Article } from './ArticleSections/Article'
 
-import "../../index.css"
-import {Course} from './CourseSections/Course'
-
-const CourseList = () => {
+const ArticleNews = () => {
   return (
-    <div className="w-100   flex flex-col gap-5 ">
+
+    <div className="w-100   flex flex-col gap-5 mt-20 ">
       <div className="w-full h-16  flex flex-row-reverse justify-between items-center  pr-[170px] pl-[170px]">
-        <div className=''>لیست دوره ها</div>
+        <div className=''>آخرین اخبار و مقالات</div>
         <div className='w-[110px] h-10 bg-amber-400 flex justify-center items-center rounded-full text-sm'>مشاهده بیشتر</div>
       </div>
 
     <div className="w-100 h-[400px]  flex justify-center items-center gap-9 ">
       
-      <Course/>
-      <Course/>
-      <Course/>
-      <Course/>
+<Article/>
+<Article/>
+<Article/>
+<Article/>
 
     </div>
     </div>
+
+
   )
 }
 
-export  {CourseList}
+export  {ArticleNews}
--- dalton_brothers/src/Screens/Landing/Landing.jsx
+++ dalton_brothers/src/Screens/Landing/Landing.jsx
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react'
 import { Header } from '../../Components/Header/Header'
 import { CourseList } from '../../Components/Course/CourseList'
+import { ArticleNews } from '../../Components/Article & News/Article & News'
 
 const Landing = () => {
   return (
 <Fragment>
 <Header/>
 <CourseList/>
-
+<ArticleNews/>
 
 </Fragment>
 
