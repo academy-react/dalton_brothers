2c44e981a7f976b31d10330dc5674942880738ee
mamjooj <mjsa4371@gmail.com>
2023-10-10 15:11:05 +0330

    adsad

--- dalton_brothers/src/Components/Landing/Header/Header.jsx
+++ dalton_brothers/src/Components/Landing/Header/Header.jsx
@@ -6,17 +6,14 @@ import { HeaderContent } from "./HeaderSections/HeaderContent";
 import styled from "./Header.module.css";
 
 import HeaderSearch from "./HeaderSections/HeaderSearch";
-import { Container } from "../../Container/Container";
 
 const Header = () => {
   return (
-    <Container>
-      <div className={styled.header}>
-        <HeaderNavbar />
-        <HeaderContent />
-        <HeaderSearch />
-      </div>
-    </Container>
+    <div className={styled.header}>
+      <HeaderNavbar />
+      <HeaderContent />
+      <HeaderSearch />
+    </div>
   );
 };
 
