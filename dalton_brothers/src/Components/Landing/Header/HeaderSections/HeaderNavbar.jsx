7324c204734097283a5f2b3ed791cedd95a8484b
mamjooj <mjsa4371@gmail.com>
2023-10-15 23:17:00 +0330

    ds

--- dalton_brothers/src/Components/Landing/Header/HeaderSections/HeaderNavbar.jsx
+++ dalton_brothers/src/Components/Landing/Header/HeaderSections/HeaderNavbar.jsx
@@ -7,12 +7,12 @@ import NavLinks from "../../../Common/Links";
 const HeaderNavbar = () => {
   return (
     <div className="pt-[10px] flex items-end font-sha">
-      <div className="w-[90%] max-w-[2000px] m-auto whitespace-nowrap flex flex-row flex-wrap justify-between items-center ">
-        <div className="w-[70px] ">
-          <img src={logo} alt="" className="w-full h-full" />
+      <div className="w-[95%] max-w-[2000px] m-auto whitespace-nowrap flex flex-row flex-wrap justify-between">
+        <div>
+          <img src={logo} alt="" className="w-[80px]" />
         </div>
-        <div className="flex flex-row flex-nowrap items-center justify-start gap-[80px] text-[#fdb501] text-base">
-          <NavLinks text={"دوره ها"} path={"/couese"} />
+        <div className="flex flex-row flex-nowrap items-center justify-start text-[#fdb501] text-xl">
+          <NavLinks text={"دوره ها"} path={"/course"} />
           <NavLinks text={"پشتیبانی"} path={"/"} />
           <NavLinks text={"پشتیبانی"} path={"/"} />
         </div>
@@ -26,6 +26,7 @@ const HeaderNavbar = () => {
             type={"button"}
             text={"ورود/ثبت نام"}
             style={"bg-[#fff] text-[#707070] hover:bg-[#ffefc8]"}
+            path={"/signIn"}
           />
         </div>
       </div>
