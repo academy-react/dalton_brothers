c2da5657de31b8368e2f56d50a5126272a35bf0a
amin <amminn.ghaderi@gmail.com>
2023-10-17 20:19:30 +0330

    re-design

--- dalton_brothers/src/Components/Landing/Category/CategorySections/CategoryTitle.jsx
+++ dalton_brothers/src/Components/Landing/Category/CategorySections/CategoryTitle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CategoryTitle = () => {
   return (
-    <div className="m-auto text-3xl h-28 flex justify-center text-[#fcbf49] items-center font-vaz">
+    <div className="m-auto text-3xl h-28 flex justify-center text-[#fcbf49] items-center font-irSans">
       دسته بندی ها
     </div>
   );
