29e05ed49834d9df9d67493d18c6967c86370dae
mamjooj <mjsa4371@gmail.com>
2023-10-17 14:55:56 +0330

    zX

--- dalton_brothers/src/Components/Article & News/Common/LayOut/Footer/FooterSection/Footer-text.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FooterText = ({ text }) => {
-  return <span className="font-est text-[#715656] text-lg whitespace-nowrap">{text}</span>;
-};
-
-export default FooterText;
