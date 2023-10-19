d429b5c037d29256dea5f4b6bcaff00c4b4dcc33
mamjooj <mjsa4371@gmail.com>
2023-10-19 11:24:53 +0330

    qw

--- dalton_brothers/src/Components/ForgetPass/ForgetPass.jsx
+++ dalton_brothers/src/Components/ForgetPass/ForgetPass.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
+import { SignInForm } from "../SignIn/SignInSection/SignInForm";
+
 import style from "./ForgetPass.module.css";
 
 const ForgetPass = () => {
   return (
-    <div className="w-100% max-w-[2000px] h-[700px] m-auto flex border-2 border-black mt-[500px]">
+    <div className="w-100% max-w-[2000px] h-[700px] m-auto flex border-2 relative overflow-hidden">
       <div className={style.top}></div>
-      <div>meow meow nigga</div>
+      <div className="w-[100%] flex justify-center items-center relative z-10">
+        <SignInForm />
+      </div>
       <div className={style.bot}></div>
     </div>
   );
