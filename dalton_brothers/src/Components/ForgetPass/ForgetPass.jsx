15ea7f6be91ea4032ce470b15610e0e80c91f76f
mamjooj <mjsa4371@gmail.com>
2023-10-19 14:10:29 +0330

    dasdsa

--- dalton_brothers/src/Components/ForgetPass/ForgetPass.jsx
+++ dalton_brothers/src/Components/ForgetPass/ForgetPass.jsx
@@ -1,17 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { SignInForm } from "../SignIn/SignInSection/SignInForm";
+import { ForgetForm } from "./ForgetSection/ForgetForm";
 
 import style from "./ForgetPass.module.css";
 
 const ForgetPass = () => {
+  const [first, setFirst] = useState(true);
+  const [toggle, setToggle] = useState(false);
+  const [back, setBack] = useState(false);
+
+  const navigate = useNavigate();
+
+  const animate = () => {
+    setTimeout(() => {
+      setFirst(!first);
+    }, 1500);
+  };
+  useEffect(() => {
+    animate();
+  }, []);
+
+  if (first)
+    return (
+      <div className="w-100% max-w-[2000px] h-[700px] m-auto flex border-2 relative overflow-hidden">
+        <div className={style.topMoveDown}></div>
+        <div className="w-[100%] flex justify-center items-center">
+          <ForgetForm zIndex={"-z-10"} />
+        </div>
+        <div className={style.botMoveDown}></div>
+      </div>
+    );
+  if (back)
+    return (
+      <div className="w-100% max-w-[2000px] h-[700px] m-auto flex border-2 relative overflow-hidden">
+        <div className={style.topMoveUp}></div>
+        <div className="w-[100%] flex justify-center items-center">
+          <ForgetForm zIndex={"-z-10"} />
+          {setTimeout(() => {
+            navigate("/signIn");
+          }, 1000)}
+        </div>
+        <div className={style.botMoveUp}></div>
+      </div>
+    );
+
   return (
     <div className="w-100% max-w-[2000px] h-[700px] m-auto flex border-2 relative overflow-hidden">
-      <div className={style.top}></div>
-      <div className="w-[100%] flex justify-center items-center relative z-10">
-        <SignInForm />
-      </div>
-      <div className={style.bot}></div>
+      {toggle ? (
+        <>
+          <div className={style.topMoveUp}></div>
+          <div className="w-[100%] flex justify-center items-center">
+            <ForgetForm zIndex={"-z-10"} />
+          </div>
+          <div className={style.botMoveUp}></div>
+        </>
+      ) : (
+        <>
+          <div className={style.top}></div>
+          <div className="w-[100%] flex justify-center items-center">
+            <ForgetForm
+              zIndex={"z-10"}
+              toggle={toggle}
+              setToggle={setToggle}
+              back={back}
+              setBack={setBack}
+            />
+          </div>
+          <div className={style.bot}></div>
+        </>
+      )}
     </div>
   );
 };
