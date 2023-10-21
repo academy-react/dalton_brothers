6f5cc8d4429fd96b47af993bff75d4e34e8840b7
mamjooj <mjsa4371@gmail.com>
2023-10-21 17:24:58 +0330

    dsadas

--- dalton_brothers/src/Components/Common/Comment/index.jsx
+++ dalton_brothers/src/Components/Common/Comment/index.jsx
@@ -3,8 +3,6 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as yup from "yup";
 
 import { Button } from "../../Common/buttons";
-import { Input } from "../Inputs/Input";
-import { Title } from "../Title/Title";
 
 const CommentPlace = () => {
   // validation................................
@@ -34,34 +32,44 @@ const CommentPlace = () => {
         onSubmit={(values) => onSubmit(values)}
         validationSchema={validation}
       >
-        <Form className="flex flex-col items-center">
+        <Form className="flex w-[500px] flex-col items-center">
           <div className="flex flex-col w-full relative  sm:w-3/4 lg:w-full mt-[30px] mb-[30px] px-[40px]">
-            <span className="mx-[20%]  peer-focus:right-[65%]  whitespace-nowrap bg-white absolute right-5 px-2 w-fit -top-4 transition-all duration-1000 text-[#595959] max-xl:bg-[#fcbf49]">
+            <span className="mx-[20%]  peer-focus:right-[65%]  whitespace-nowrap bg-white absolute right-5 px-2 w-fit -top-4 transition-all duration-1000 text-[#595959] max-xl:bg-[#fcbf49] max-xl:text-[#fff]">
               نام کاربر
             </span>
             <Field
               name="name"
-              className=" w-[85%] m-auto text-right h-[40px] rounded-full outline-none border-zinc-300 border-[2px] peer px-5 max-xl:bg-[#fcbf49] max-xl:border-gray-50 "
+              className=" w-[85%] m-auto text-right h-[40px] rounded-full outline-none border-zinc-300 border-[2px] peer px-5 max-xl:bg-[#fcbf49] max-xl:border-gray-50 max-xl:text-[#fff] max-xl:placeholder:text-[#fff]"
               type="text"
               placeholder="...نام کاربری"
             />
-            <ErrorMessage name="name" component={"div"} className="error font-irSans text-center mt-[10px]" />
+            <ErrorMessage
+              name="name"
+              component={"div"}
+              className="error font-irSans text-center mt-[10px]"
+            />
           </div>
           <div className="flex flex-col w-full relative  sm:w-3/4 lg:w-full mb-[30px] px-[40px]">
-            <span className=" mx-[20%]  peer-focus:right-[65%]  whitespace-nowrap bg-white absolute right-5 px-2 w-fit -top-4 transition-all duration-1000 text-[#595959] max-xl:bg-[#fcbf49] ">
+            <span className=" mx-[20%]  peer-focus:right-[65%]  whitespace-nowrap bg-white absolute right-5 px-2 w-fit -top-4 transition-all duration-1000 text-[#595959] max-xl:bg-[#fcbf49] max-xl:text-[#fff] ">
               پیام شما
             </span>
             <Field
               as="textarea"
               name="comment"
-              className=" w-[85%] m-auto text-right min-h-[120px] max-h-[150px] rounded-[20px] outline-none border-zinc-300 border-[2px] peer px-5 py-[15px] max-xl:bg-[#fcbf49] text-stone-300 max-xl:border-gray-50"
+              className=" w-[85%] m-auto text-right min-h-[120px] max-h-[150px] rounded-[20px] outline-none border-zinc-300 border-[2px] peer px-5 py-[15px] max-xl:bg-[#fcbf49] max-xl:border-gray-50 max-xl:text-[#fff] max-xl:placeholder:text-[#fff]"
               type="text"
               placeholder="...متن پیام"
             />
-            <ErrorMessage name="comment" component={"div"} className="error  font-irSans text-center mt-[10px]" />
+            <ErrorMessage
+              name="comment"
+              component={"div"}
+              className="error  font-irSans text-center mt-[10px]"
+            />
           </div>
           <Button
-            style={"bg-[#fcbf49] hover:bg-[#c89c44] text-[#fff] max-xl:bg-[#fff] max-xl:text-[#fcbf49]"}
+            style={
+              "bg-[#fcbf49] hover:bg-[#c89c44] text-[#fff] max-xl:bg-[#fff] max-xl:text-[#fcbf49]"
+            }
             type={"submit"}
             text={"ثبت"}
           />
