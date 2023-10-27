987e5bd638a0b0619385425f40af606ef6a6b69e
amin <amminn.ghaderi@gmail.com>
2023-10-27 12:29:20 +0330

    re-design

--- dalton_brothers/src/Components/StudentPanel/PanelCourses/Component/MyCourse/MyCourse.jsx
+++ dalton_brothers/src/Components/StudentPanel/PanelCourses/Component/MyCourse/MyCourse.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import { IconTrash, IconPlus } from "@tabler/icons-react";
 import { useLocation } from "react-router-dom";
 
+import delIcon from "../../../../../assets/Images/panel/panelDel.png";
+import addlIcon from "../../../../../assets/Images/panel/PanelAdd.png";
+
 const MyCourse = ({
   coursePic,
   courseName,
@@ -13,13 +16,13 @@ const MyCourse = ({
 }) => {
   const location = useLocation();
   return (
-    <div className="bg-gray-100 rounded-[28px] w-full lg:h-[65px] h-[55px] xl:my-3 my-1 flex flex-row-reverse justify-evenly items-center text-gray-800  whitespace-nowrap ">
-      <div className="lg:px-[65px] px-[30px] lg:w-[80px] w-[50px] h-[65px] text-center text-xl">
+    <div className="bg-[#f1f5f9] rounded-[28px] w-full lg:h-[65px] h-[55px] xl:my-3 my-1 flex flex-row-reverse justify-evenly items-center text-gray-800  whitespace-nowrap ">
+      <div className=" w-[50px] h-[65px] text-center rounded-full flex justify-center items-center mr-5">
         {" "}
         <img
           src={coursePic}
           alt=""
-          className="bg-red-200 w-[40px] h-[40px]"
+          className="bg-red-200 w-[40px] h-[40px] rounded-full"
         ></img>{" "}
       </div>
       <div className="lg:w-[130px] w-[205px]  h-[25px] text-center "> {courseName} </div>
@@ -29,16 +32,22 @@ const MyCourse = ({
       <div className="lg:w-[130px] w-[205px]  h-[25px] text-center "> {price} </div>
       <div className="lg:w-[48px]  xl:w-[60px] w-[100px] xl:h-[55px] h-[40px] flex justify-center items-center">
         {location.pathname === "/panel/PanelCourses" ? (
-          <IconTrash
-            strokeWidth="1"
-            className="w-[80%] h-[80%] rounded-full bg-orange-300"
-          />
+          <div className="w-[40px] h-[40px] rounded-full bg-orange-300 flex justify-center items-center cursor-pointer">
+          <img
+            src={delIcon}
+            className="w-[25px] h-[25px]"
+          /></div>
         ) : (
-          <IconPlus
-            strokeWidth="1"
-            className="w-[80%] h-[80%] rounded-full bg-orange-300"
+          <div className="w-[40px] h-[40px] rounded-full bg-orange-300 flex justify-center items-center cursor-pointer">
+          
+          <img
+          src={addlIcon}
+            className="w-[25px] h-[25px]"
           />
+          </div>
         )}
+
+
       </div>
     </div>
   );
--- dalton_brothers/src/Components/StudentPanel/PanelCoursesList/PanelCoursesList.jsx
+++ dalton_brothers/src/Components/StudentPanel/PanelCoursesList/PanelCoursesList.jsx
@@ -8,15 +8,15 @@ import { Pagination } from './Pagination/Pagination'
 
 const PanelCoursesList = () => {
   return (
-    <div className=' w-full h-full flex flex-col pt-7 px-2'>
+    <div className=' w-full h-full flex flex-col pt-7 px-2 font-irSans'>
     <div className=' w-full xl:h-[100px] h-[60px] flex justify-evenly items-center flex-row-reverse xl:text-xl text-lg whitespace-nowrap'>
-      <div className='w-[130px] xl:h-[50px] h-[40px] text-center '> تصویر </div>
-      <div className='w-[130px] xl:h-[50px] h-[40px] text-center '> نام دوره </div>
-      <div className='w-[130px] xl:h-[50px] h-[40px] text-center '> مدرس </div>
-      <div className='w-[130px] xl:h-[50px] h-[40px] text-center '> ترم </div>
-      <div className='w-[130px] xl:h-[50px] h-[40px] text-center '> تاریخ شروع </div>
-      <div className='w-[130px] xl:h-[50px] h-[40px] text-center '> قیمت </div>
-      <div className='w-[60px] xl:h-[50px] h-[40px] text-center'>  </div>
+      <div className='w-[130px] xl:h-[50px] h-[40px] text-center text-gray-600 '> تصویر </div>
+      <div className='w-[130px] xl:h-[50px] h-[40px] text-center text-gray-600'> نام دوره </div>
+      <div className='w-[130px] xl:h-[50px] h-[40px] text-center text-gray-600'> مدرس </div>
+      <div className='w-[130px] xl:h-[50px] h-[40px] text-center text-gray-600'> ترم </div>
+      <div className='w-[130px] xl:h-[50px] h-[40px] text-center text-gray-600'> تاریخ شروع </div>
+      <div className='w-[130px] xl:h-[50px] h-[40px] text-center text-gray-600'> قیمت </div>
+      <div className='w-[60px] xl:h-[50px] h-[40px] text-center text-gray-600'>  </div>
     </div>
     <div className='w-full h-[500px] pt-3 flex flex-col gap-1'>
       {allCourseData.map((item,index) =>( 
