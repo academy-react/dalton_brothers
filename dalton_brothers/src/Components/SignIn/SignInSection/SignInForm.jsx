9adf8c491f7d11838a302e7d7b6158230c221261
mamjooj <mjsa4371@gmail.com>
2023-11-09 11:19:21 +0330

    adsa]

--- dalton_brothers/src/Components/SignIn/SignInSection/SignInForm.jsx
+++ dalton_brothers/src/Components/SignIn/SignInSection/SignInForm.jsx
@@ -34,131 +34,130 @@ const SignInForm = () => {
 
     setItem("token", user.token);
   };
-};
 
-const postLogin = async (item) => {
-  try {
-    const result = await axios.post(
-      "https://api-academy.iran.liara.run/api/Sign/Login",
-      item
-    );
-    console.log(result);
-  } catch (error) {
-    alert("haaaaa", error);
-  }
-  alert("nale kon");
-};
+  const postLogin = async (item) => {
+    try {
+      const result = await axios.post(
+        "https://api-academy.iran.liara.run/api/Sign/Login",
+        item
+      );
+      console.log(result);
+    } catch (error) {
+      alert("haaaaa", error);
+    }
+    alert("nale kon");
+  };
 
-// validation................................
-const validation = yup.object().shape({
-  logInPassword: yup.string().required("این فیلد اجباریست"),
-  logInUserName: yup.string().required("این فیلد اجباریست"),
-});
+  // validation................................
+  const validation = yup.object().shape({
+    logInPassword: yup.string().required("این فیلد اجباریست"),
+    logInUserName: yup.string().required("این فیلد اجباریست"),
+  });
 
-return (
-  <div
-    className={`2xl:w-[40%] xl:w-1/2 lg:w-3/5 md:w-[80%] w-full relative flex flex-row justify-center items-center`}
-  >
-    <Formik
-      initialValues={{
-        logInPassword: "",
-        logInUserName: "",
-        remember: "true",
-      }}
-      onSubmit={handleToggle}
-      validationSchema={validation}
+  return (
+    <div
+      className={`2xl:w-[40%] xl:w-1/2 lg:w-3/5 md:w-[80%] w-full relative flex flex-row justify-center items-center`}
     >
-      <Form className=" w-full flex flex-col justify-center items-center  gap-[20px] px-10 rounded-[30px]">
-        <Title
-          topic={"صفحه ورود"}
-          style={"leading-3 text-[20px] self-center text-[#9a9a9a]"}
-        />
-        <div className="min-[500px]:w-[80%] w-full">
-          <Input
-            topic={"نام کاربر"}
-            className={"rounded-full"}
-            placeHolder={"... نام کاربری خود را وارد کنید"}
-            type={"text"}
-            name={"logInUserName"}
-            as={"input"}
+      <Formik
+        initialValues={{
+          logInPassword: "",
+          logInUserName: "",
+          remember: "true",
+        }}
+        onSubmit={handleToggle}
+        validationSchema={validation}
+      >
+        <Form className=" w-full flex flex-col justify-center items-center  gap-[20px] px-10 rounded-[30px]">
+          <Title
+            topic={"صفحه ورود"}
+            style={"leading-3 text-[20px] self-center text-[#9a9a9a]"}
           />
-        </div>
-        <div className="relative min-[500px]:w-[80%] w-full flex justify-end">
-          <Input
-            topic={"رمز عبور"}
-            className={"rounded-full"}
-            placeHolder={""}
-            type={show ? "text" : "password"}
-            name={"logInPassword"}
-            as={"input"}
-          />
-          <div className="w-[25px] absolute left-[8%] top-[14%]">
-            {show ? (
-              <TbEye
-                onClick={() => setShow(!show)}
-                className="w-full h-full opacity-50"
-              />
-            ) : (
-              <TbEyeOff
-                onClick={() => setShow(!show)}
-                className="w-full h-full opacity-50"
-              />
-            )}
+          <div className="min-[500px]:w-[80%] w-full">
+            <Input
+              topic={"نام کاربر"}
+              className={"rounded-full"}
+              placeHolder={"... نام کاربری خود را وارد کنید"}
+              type={"text"}
+              name={"logInUserName"}
+              as={"input"}
+            />
           </div>
-        </div>
-        <div className="pr-[70px] flex flex-row-reverse items-center self-end gap-[10px] h-12">
-          <Input
-            type={"checkbox"}
-            name={"remember"}
-            id="remember"
-            className="w-5 h-5 justify-center "
-            as={"input"}
-            onClick={() => setRemember(!remember)}
+          <div className="relative min-[500px]:w-[80%] w-full flex justify-end">
+            <Input
+              topic={"رمز عبور"}
+              className={"rounded-full"}
+              placeHolder={""}
+              type={show ? "text" : "password"}
+              name={"logInPassword"}
+              as={"input"}
+            />
+            <div className="w-[25px] absolute left-[8%] top-[14%]">
+              {show ? (
+                <TbEye
+                  onClick={() => setShow(!show)}
+                  className="w-full h-full opacity-50"
+                />
+              ) : (
+                <TbEyeOff
+                  onClick={() => setShow(!show)}
+                  className="w-full h-full opacity-50"
+                />
+              )}
+            </div>
+          </div>
+          <div className="pr-[70px] flex flex-row-reverse items-center self-end gap-[10px] h-12">
+            <Input
+              type={"checkbox"}
+              name={"remember"}
+              id="remember"
+              className="w-5 h-5 justify-center "
+              as={"input"}
+              onClick={() => setRemember(!remember)}
+            />
+            <label
+              htmlFor="remember"
+              className="font-irSans font-thin text-[#939393] text-sm whitespace-nowrap"
+            >
+              مرا به خاطر بسپار
+            </label>
+          </div>
+          <Button
+            type={"submit"}
+            className={
+              "bg-[#fcbf49] w-[60%] py-[5px] text-[13px] font-irSans text-[#595959]"
+            }
+            value={"ورود"}
           />
-          <label
-            htmlFor="remember"
-            className="font-irSans font-thin text-[#939393] text-sm whitespace-nowrap"
-          >
-            مرا به خاطر بسپار
-          </label>
-        </div>
-        <Button
-          type={"submit"}
-          className={
-            "bg-[#fcbf49] w-[60%] py-[5px] text-[13px] font-irSans text-[#595959]"
-          }
-          value={"ورود"}
-        />
-        <NavLinks
-          className={
-            "bg-[#fcbf49] w-[60%] py-[5px] text-[13px] font-irSans text-lg  text-[#595959]"
-          }
-          Children={"خانه"}
-          path={"/"}
-        />
-        <div className="self-end pr-[30px] sm:pr-[80px] whitespace-nowrap flex flex-row-reverse items-center ">
-          <span className="text-[#868686] font-thin text-[14px] font-irSans">
-            ایا رمز عبور خود را فراموش کردبد؟
-          </span>
           <NavLinks
-            Children={"بلی"}
-            className="text-[#fcbf49] font-thin text-[14px] font-irSans py-[0]"
-            path={"/forget"}
+            className={
+              "bg-[#fcbf49] w-[60%] py-[5px] text-[13px] font-irSans text-lg  text-[#595959]"
+            }
+            Children={"خانه"}
+            path={"/"}
           />
-        </div>
-        <div className="self-end pr-[30px] sm:pr-[80px] whitespace-nowrap flex flex-row-reverse items-center ">
-          <span className="text-[#868686] font-thin text-[14px] font-irSans">
-            حساب فعال ندارید؟
-          </span>
-          <NavLinks
-            Children={"ایجاد حساب"}
-            className="text-[#fcbf49] font-thin text-[14px] font-irSans py-[0]"
-            path={"/register"}
-          />
-        </div>
-      </Form>
-    </Formik>
-  </div>
-);
-
+          <div className="self-end pr-[30px] sm:pr-[80px] whitespace-nowrap flex flex-row-reverse items-center ">
+            <span className="text-[#868686] font-thin text-[14px] font-irSans">
+              ایا رمز عبور خود را فراموش کردبد؟
+            </span>
+            <NavLinks
+              Children={"بلی"}
+              className="text-[#fcbf49] font-thin text-[14px] font-irSans py-[0]"
+              path={"/forget"}
+            />
+          </div>
+          <div className="self-end pr-[30px] sm:pr-[80px] whitespace-nowrap flex flex-row-reverse items-center ">
+            <span className="text-[#868686] font-thin text-[14px] font-irSans">
+              حساب فعال ندارید؟
+            </span>
+            <NavLinks
+              Children={"ایجاد حساب"}
+              className="text-[#fcbf49] font-thin text-[14px] font-irSans py-[0]"
+              path={"/register"}
+            />
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
 export { SignInForm };
