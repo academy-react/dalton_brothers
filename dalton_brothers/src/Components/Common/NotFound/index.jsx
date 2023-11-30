59946456535b1938cddd418ddfe511529d84854b
amin <amminn.ghaderi@gmail.com>
2023-11-30 22:23:44 +0330

    403

--- dalton_brothers/src/Components/Common/NotFound/index.jsx
+++ dalton_brothers/src/Components/Common/NotFound/index.jsx
@@ -11,20 +11,26 @@ const NotFound = () => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <div className="w-[100-vw] h-[100vh] bg-white flex items-end  flex-col ">
+      <div className="w-[100-vw] h-[100vh] bg-white flex items-end  flex-col font-irSans ">
         <img
           className="w-[100-vw] h-[100vh] absolute bottom-0 left-0 z-10"
           src={NotFoundPage}
           alt=""
         />
 
-        <div className=" h-28   flex flex-row-reverse flex-nowrap items-center justify-center gap-5 text-[#fdb501] text-lg z-20 mr-8 ">
+        <div className=" h-28   flex flex-row-reverse flex-nowrap items-center justify-center gap-5 text-mode-700 text-lg z-20 mr-8 ">
           <NavLinks Children={"خانه"} path={"/"} />
 
           <NavLinks Children={"دوره ها"} path={"/course"} />
           <NavLinks Children={"  اخبار و مقالات"} path={"/news"} />
         </div>
-        <div className="w-10 h-60 bg-red-100 z-20"></div>
+        <div className="w-1/2 h-60  z-20 mt-40 flex flex-col justify-center  items-center gap-10 text-2xl text-mode-700 "> 
+        صفحه مورد نظر یافت نشد 
+        
+        <NavLinks className={"bg-pallete-100 text-white text-base"} Children={" بازگشت به صفحه اصلی"} path={"/"} />
+
+
+        </div>
 
       </div>
     </motion.div>
