253fd93bc150eb39681548a7c804da0b59f28442
amin <amminn.ghaderi@gmail.com>
2023-12-02 22:00:47 +0330

    nightModeSignIn

--- dalton_brothers/src/Components/SignIn/SignIn.jsx
+++ dalton_brothers/src/Components/SignIn/SignIn.jsx
@@ -6,7 +6,7 @@ const SignIn = () => {
   return (
     <motion.div
       dir="ltr"
-      className={`xl:bg-[url('../../../src/assets/Images/signIn-bg.svg')] xl:justify-start md:px-[50px] justify-center bg-no-repeat bg-cover bg-center w-[100%] h-[100vh] pt-[100px]  flex flex-nowrap items-start `}
+      className={`xl:bg-[url('../../../src/assets/Images/signIn-bg.svg')] xl:dark:bg-[url('../../../src/assets/Images/darkSignIn.svg')]     xl:justify-start md:px-[50px] justify-center bg-no-repeat bg-cover bg-center w-[100%] h-[100vh] pt-[100px]  flex flex-nowrap items-start `}
            initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
