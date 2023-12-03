f388041116f3148eea6623f7e8e62f91b9cc3e1c
amin <amminn.ghaderi@gmail.com>
2023-12-03 18:21:17 +0330

    nightModeAuth

--- dalton_brothers/src/Components/ForgetPass/ForgetSection/ForgetForm/ForgetForm.jsx
+++ dalton_brothers/src/Components/ForgetPass/ForgetSection/ForgetForm/ForgetForm.jsx
@@ -10,9 +10,14 @@ import { Button } from "../../../Common/buttons";
 import { NavLinks } from "../../../Common/Links/NavLinks/NavLinks";
 
 import logo from "../../../../assets/Images/logo.png";
+import darkLogo from "../../../../assets/Images/mode-logo.png";
+import { useSelector } from "react-redux";
 
 const ForgetForm = () => {
   const navigate = useNavigate();
+
+  const colorMode = useSelector((state) => state.theme.theme);
+
 
   const validation = yup.object().shape({
     email: yup.string().required("این فیلد اجباریست"),
@@ -30,13 +35,13 @@ const ForgetForm = () => {
       >
         <Form className={`flex flex-col gap-[35px] items-center relative `}>
           <div className="w-[75px]">
-            <img src={logo} alt="" className="w-full h-full" />
+            <img src={colorMode === "dark" ? darkLogo :  logo} alt="" className="w-full h-full" />
           </div>
-          <Title topic={"فراموشی رمز عبور"} style={"text-[#989898]"} />
+          <Title topic={"فراموشی رمز عبور"} style={"text-[#989898] dark:text-mode-50"} />
           <div className="w-[400px]">
             <Input
               topic={"ایمیل"}
-              className={"rounded-full w-full border-[#bcbcbc] border-[2px]"}
+              className={"rounded-full w-full border-[#bcbcbc] border-2 dark:focus:border-DarkPallete-100 dark:text-mode-50 dark:bg-mode-900"}
               placeHolder={"...ایمیل خود را وارد کنید"}
               type={"text"}
               name={"email"}
@@ -46,13 +51,13 @@ const ForgetForm = () => {
           <Button
             type={"submit"}
             className={
-              "py-[7px] px-[60px] bg-[#fcbf49] font-irSans text-[13px] text-[#232323]"
+              "py-[7px] px-[60px] bg-[#fcbf49] font-irSans text-[13px] text-mode-50 dark:bg-DarkPallete-100"
             }
             value={"مرحله بعد"}
             onClick={() => navigate("/forget")}
           />
           <NavLinks
-            className="font-irSans text-[13px] text-[#989898] mt-[-20px]"
+            className="font-irSans text-[13px] text-[#989898] mt-[-20px] dark:text-mode-100"
             Children={"بازگشت"}
             path={"/signIn"}
           />
