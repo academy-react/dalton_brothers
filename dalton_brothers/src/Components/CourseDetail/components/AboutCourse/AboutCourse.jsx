1c80ac74d11e677501f2fbeb1c973a646fd79b38
amin <amminn.ghaderi@gmail.com>
2023-12-07 13:21:54 +0330

    detailsReDesign

--- dalton_brothers/src/Components/CourseDetail/components/AboutCourse/AboutCourse.jsx
+++ dalton_brothers/src/Components/CourseDetail/components/AboutCourse/AboutCourse.jsx
@@ -6,6 +6,8 @@ import userGold from "../../../../../src/assets/Images/user-gold-2.png";
 import AboutCourseImg from "../../../../../src/assets/Images/icons8-python-144.png";
 import { reserveCourse } from "../../../../Core/Services/api/course/reserve";
 
+import courseDetailImage from "../../../../assets/Images/courseDetailImage.jpg";
+
 const AboutCourse = ({
   title,
   teacherName,
@@ -27,84 +29,50 @@ const AboutCourse = ({
     console.log(result);
   };
   return (
-    <div>
-      <div className="bg-[#F1F5F9] w-[500px] h-[390px] rounded-[25px] flex justify-center items-center mx-auto mt-[100px] relative lg:w-[950px] md:w-[700px] ">
-        {/* <div className=" md:w-[190px] md:h-[190px] absolute -top-[99px] w-[150px] h-[150px]">
-          <img src={AboutCourseImg } alt="" className="w-full h-full"></img>
-        </div> */}
-        <div className=" w-1/2 h-[300px] flex flex-col items-center justify-center">
-          <div className=" md:w-[200px] md:h-[200px] h-[150px] w-[150px] flex justify-center items-center ">
-            <img src={imageAddress }  alt="" className="w-3/4 h-3/4"></img>
+    <div className="h-[500px] bg-pallete-50 w-full mt-10 flex justify-center items-center p-10  ">
+      <div className="w-3/5 h-full bg-slate-600 "></div>
+      <div className="w-2/5 h-full  bg-red-700">
+        <div className="h-[70%] bg-yellow-400 flex justify-center items-center">
+          {" "}
+          <img
+            className="w-4/5 h-full rounded-[30px] flex justify-center items-center"
+            src={courseDetailImage}
+            alt="عکسی وجود ندارد"
+          />
+        </div>
+        <div className="h-[30%] w-[80%] m-auto flex justify-between items-center flex-row-reverse ">
+          <div className="w-full flex justify-end">
+            <button
+              className="  bg-[#fcbf49] md:text-xl text-md px-10 py-2 rounded-[40px] text-white font-irSans"
+              onClick={() => handleClick()}
+            >
+              ثبت نام
+            </button>
           </div>
-          <div className="flex justify-center w-full p-[15px] text-center mt-[10px] lg:text-2xl  md:text-xl  text-lg text-gray-800 font-irSans">
-            <div className=" pr-2">{teacherName}</div>
-            <div className="text-gray-500">: مدرس </div>
-          </div>
-          <div className="flex justify-end text-right lg:text-2xl  md:text-xl  text-lg text-gray-800 font-irSans">
-            <p className="text-right  pt-[2px] pr-3">
-              {" "}
-              {currentRate}{" "}
-            </p>
-            <p className="text-right text-gray-500"> : امتیاز دوره </p>
+          <div className="h-full w-full flex justify-center items-center gap-7">
+            <div className="w-14 h-14 bg-white rounded-full">
+              <img src="" alt="" />
+            </div>
+            <div className="flex justify-center items-center gap-2">
+              <div className="w-20 h-12 bg-white rounded-l-[100px] rounded-r-[20px]">
+                <img src="" alt="" />
+              </div>
+              <div className="w-20 h-12 bg-white rounded-r-[100px] rounded-l-[20px] ">
+                <img src="" alt="" />
+              </div>
+            </div>
           </div>
         </div>
-        <div className=" w-1/2 h-[350px] flex flex-col justify-evenly gap-[5px] md:gap-[15px]">
-          <h3 className=" lg:text-3xl  md:text-2xl  text-xl text-right pr-[40px] mt-4 font-irSBold">
-            {" "}
-            {title}{" "}
-          </h3>
-          <div className="flex justify-end text-right pr-[60px] mt-4 lg:text-2xl  md:text-xl  text-lg text-gray-800 font-irSans">
-            <p className="text-right  pt-[2px] pr-3  font-irSans">
-              {" "}
-              {courseLevelName}{" "}
-            </p>
-            <p className="text-right text-gray-500 font-irSans"> : سطح دوره </p>
-          </div>
-          <div className="flex justify-end text-right pr-[60px]  lg:text-2xl  md:text-xl  text-lg text-gray-800  font-irSans">
-            <p className="text-right  pt-[2px] pr-3">
-              {" "}
-              {courseStatusName}{" "}
-            </p>
-            <p className="text-right text-gray-500"> : وضعیت برگزاری </p>
-          </div>
-          <div className="flex justify-end text-right pr-[60px]  lg:text-2xl  md:text-xl  text-lg text-gray-800 font-irSans">
-            <p className="text-right  pt-[2px] pr-3">
-              {" "}
-              {startTime}{" "}
-            </p>
-            <p className="text-right text-gray-500"> : تاریخ شروع دوره  </p>
-          </div>
-          <div className="flex justify-end text-right pr-[60px]  lg:text-2xl  md:text-xl  text-lg text-gray-800 font-irSans">
-            <p className="text-right pt-[2px] pr-3">
-              {" "}
-              {endTime}{" "}
-            </p>
-            <p className="text-right text-gray-500"> : پایان دوره  </p>
-          </div>
-          <div className="flex justify-end text-right pr-[60px]  lg:text-2xl  md:text-xl  text-lg text-gray-800 font-irSans">
-            <p className="text-right pt-[2px] pr-3">
-              {" "}
-              {cost}{" "}
-            </p>
-            <p className="text-right text-gray-500"> : هزینه دوره </p>
-          </div>
-          <div className="flex justify-end text-right pr-[60px]  lg:text-2xl  md:text-xl  text-lg text-gray-800 font-irSans">
-            <p className="text-right pt-[2px] pr-3 pb-4">
-              {" "}
-              {capacity}{" "}
-            </p>
-            <p className="text-right text-gray-500"> : ظرفیت دوره </p>
-          </div>
-        </div>
-        <button
-          className="md:px-14 md:py-4 bg-[#fcbf49] md:text-xl text-md px-11 py-3 absolute -bottom-[27px] rounded-[40px] text-white font-irSans"
-          onClick={() => handleClick()}
-        >
-          ثبت نام
-        </button>
       </div>
     </div>
   );
 };
 
 export { AboutCourse };
+
+/* <button
+          className="md:px-14 md:py-4 bg-[#fcbf49] md:text-xl text-md px-11 py-3 absolute -bottom-[27px] rounded-[40px] text-white font-irSans"
+          onClick={() => handleClick()}
+        >
+          ثبت نام
+        </button>*/
