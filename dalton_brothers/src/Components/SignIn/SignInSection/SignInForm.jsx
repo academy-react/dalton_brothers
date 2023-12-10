6b629961f50a0bee780cd6387cfb5388800ab179
mamjooj <mjsa4371@gmail.com>
2023-12-10 21:15:55 +0330

    dsa

--- dalton_brothers/src/Components/SignIn/SignInSection/SignInForm.jsx
+++ dalton_brothers/src/Components/SignIn/SignInSection/SignInForm.jsx
@@ -48,11 +48,7 @@ const SignInForm = () => {
     // //console.log(user);
 
     if (user.success) {
-      const userName = await basicGet("/SharePanel/GetProfileInfo");
-      toast.success(
-        `${userName.fName ? userName.fName : "کاربر"}   عزیز به سایت خوش آمدید `
-      );
-        navigate("/");
+      navigate("/");
       dispatch(onUserChange(user));
       if (remember) {
         setItem("token", user.token);
@@ -81,6 +77,10 @@ const SignInForm = () => {
       toast.error("حسابی با این مشخصات وجود ندارد");
       return;
     }
+    const userName = await basicGet("/SharePanel/GetProfileInfo");
+    toast.success(
+      `${userName.fName ? userName.fName : "کاربر"}   عزیز به سایت خوش آمدید `
+    );
   };
 
   // validation................................
